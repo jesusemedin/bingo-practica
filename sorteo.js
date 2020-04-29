63ca9fcbcb317ec484e6a43039fe23e7bb075373
Jesus Medina <medinabotamail@gmail.com>
2020-04-28 20:13:51 -0400

    se agrego array

--- sorteo.js
+++ sorteo.js
@@ -1,46 +1,39 @@
-function sorteofichas()
+function sorteofichas ()
+{
+function random (minimo,maximo)
+{
+var numerorandom = Math.floor(( Math.random() * (maximo - minimo ) + 1) + minimo)
+return numerorandom
+}
+
+var numero = random (0,75);
+var usados = [];
+usados.push(numero);
+var letra;
+var resultado = usados;
+
+if (usados <= 15 ) 
     {
-        //Creamos funcion que genere numeros aleatorios
-        //Asignamos esos numeros aleatorios a la vaariables numeros estableciendo parametros entre 0 y 75
-
-        var numero = random(0,75);
-
-        function random(minimo,maximo)
-        {
-            var numerorandom = Math.floor(( Math.random() * (maximo - minimo ) + 1) + minimo)
-            return numerorandom
-        }
-        var letra;
-
-        //Aplicamos condiciones al programa de la siguiente manera:
-        if (numero <= 15 ) 
-        {
-            letra = "B"
-        }
-        else if (numero >= 16 && numero <=30) 
-        {
-            letra = "I"    
-        }
-        else if (numero >= 31 && numero <= 45)
-        {
-            letra = "N"
-        }
-        else if(numero >= 46 && numero <= 60)
-        {
-            letra = "G"
-        }
-        else if(numero >= 61 && numero <= 75)
-        {
-            letra = "O"
-        }
-
-        //Creamos una variable letra para almacenar los datos que se hicieron atraves de las condiciones
-
-        //Generamos una variable resultado para que sea llamada por la etiqueta
-        //document.getElementById('resultado').innerHTML = resultado;
-        // en HTML <p><div id="resultado">Aqui se mostrara el resultado</div></p>
-
-        var resultado =letra + "" + numero
-
-        document.getElementById('resultado').innerHTML = resultado;
+    letra = "B"
     }
+    else if (usados >= 16 && usados <=30)
+    {
+    letra = "I"    
+    }
+    else if (usados >= 31 && usados <= 45)
+    {
+    letra = "N"
+    }
+    else if(usados >= 46 && usados <= 60)
+    {
+    letra = "G"
+    }
+    else if(usados >= 61 && usados <= 75)
+    {
+    letra = "O"
+    }
+
+    //var resultado = letra + "" + numero
+
+    document.getElementById('resultado').innerHTML = letra + "" + resultado;
+}
