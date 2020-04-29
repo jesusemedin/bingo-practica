61dd2f647c7bf35555ef87fd5c6ee54062a5eab1
Jesus Medina <medinabotamail@gmail.com>
2020-04-29 14:15:49 -0400

    equis

--- sorteo.js
+++ sorteo.js
@@ -6,34 +6,31 @@ var numerorandom = Math.floor(( Math.random() * (maximo - minimo ) + 1) + minimo
 return numerorandom
 }
 
-var numero = random (0,75);
-var usados = [];
-usados.push(numero);
+var numeronuevo = random (0,75);
 var letra;
-var resultado = usados;
 
-if (usados <= 15 ) 
+
+if (numeronuevo <= 15 ) 
     {
     letra = "B"
     }
-    else if (usados >= 16 && usados <=30)
+    else if (numeronuevo >= 16 && numeronuevo <=30)
     {
     letra = "I"    
     }
-    else if (usados >= 31 && usados <= 45)
+    else if (numeronuevo >= 31 && numeronuevo <= 45)
     {
     letra = "N"
     }
-    else if(usados >= 46 && usados <= 60)
+    else if(numeronuevo >= 46 && numeronuevo <= 60)
     {
     letra = "G"
     }
-    else if(usados >= 61 && usados <= 75)
+    else if(numeronuevo >= 61 && numeronuevo <= 75)
     {
     letra = "O"
     }
 
-    //var resultado = letra + "" + numero
-
-    document.getElementById('resultado').innerHTML = letra + "" + resultado;
+    var resultadofinal = letra + "" + numeronuevo
+    document.getElementById('resultado').innerHTML = resultadofinal;
 }
